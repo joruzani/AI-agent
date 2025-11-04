3f4f2e17cc64b5f6a1be84881db2a1ba7f4227e1
joruzani <joruzani@gmail.com>
2025-11-03 19:04:28 -0600

    add cl prompt input

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
 
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
+    prompt = sys.argv
+    if len(prompt) > 1:
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', contents=f"{prompt[1]}"
+        )
+    else:
+        print("Need to provide a prompt")
+        sys.exit(1)
+
 
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
