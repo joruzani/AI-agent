645f54f70ca559e36e1417ad1d26c4f5c887335b
joruzani <joruzani@gmail.com>
2025-11-04 22:57:18 -0600

    added try/except

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -14,11 +14,13 @@ def get_files_info(working_directory, directory="."):
     if not abs_path.startswith(abs_work + os.sep) and abs_path != abs_work:
        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    
-    dir_contents = os.listdir(abs_path)
-    contents = "\n".join(list(map(lambda file: f"- {file}: file_size={os.path.getsize(abs_path+os.sep+file)} bytes, is_dir={os.path.isdir(abs_path+os.sep+file)}", dir_contents)))
-    if directory == ".":
-        return f"Results for current directory:\n{contents}"
-    else:
-        return f"Results for '{directory}' directory:\n{contents}"
+    try:    
+        dir_contents = os.listdir(abs_path)
+        contents = "\n".join(list(map(lambda file: f"- {file}: file_size={os.path.getsize(abs_path+os.sep+file)} bytes, is_dir={os.path.isdir(abs_path+os.sep+file)}", dir_contents)))
+        if directory == ".":
+            return f"Results for current directory:\n{contents}"
+        else:
+            return f"Results for '{directory}' directory:\n{contents}"
+    except Exception as e:
+        return f"Error listing files: {e}"
 
