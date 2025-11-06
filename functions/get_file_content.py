8371aec98dc1bbcc86d4f1b6f73f35513fc667fc
joruzani <joruzani@gmail.com>
2025-11-05 18:27:37 -0600

    get_file_content working, starting write_file

---
+++ functions/get_file_content.py
@@ -0,0 +1,23 @@
+import os
+from config import *
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    abs_work = os.path.abspath(working_directory)
+    abs_path = os.path.abspath(full_path)
+
+
+    if not abs_path.startswith(abs_work + os.sep) and abs_path != abs_work:
+       return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(abs_path) as file:
+            contents = file.read()
+            if len(contents) > CHAR_LIMIT:
+                contents = contents[:CHAR_LIMIT]
+                contents += f"[...File \"{file_path}\" truncated at 10000 characters]"
+            return contents
+    except Exception as e:
+        return f"Error: reading error {e}"
