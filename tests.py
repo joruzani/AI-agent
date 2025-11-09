c6ee38b1f312881198cc53bbe2b399bb9fb2da83
joruzani <joruzani@gmail.com>
2025-11-08 22:44:00 -0600

    run_python and its tests

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def test():
 
@@ -45,19 +46,44 @@ def test():
     #print("Result for current directory:")
     #print(result)
     #=======================================================
+    
+    # WRITE FILE TESTS ===========================================================
+    #result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum") #print("Result for current directory:")
+    #print(result)
+    #print("")
 
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print("Result for current directory:")
+    #result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    #print("Result for current directory:")
+    #print(result)
+    #print("")
+
+    #result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    #print("Result for current directory:")
+    #print(result)
+    #print("")
+
+    # RUN PYTHON TESTS ==================================================================
+    result = run_python_file("calculator", "main.py") #print("Result for current directory:")
     print(result)
     print("")
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print("Result for current directory:")
+    result = run_python_file("calculator", "main.py", ["3 + 5"]) #print("Result for current directory:")
     print(result)
     print("")
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print("Result for current directory:")
+    result = run_python_file("calculator", "test.py") #print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "../main.py") #print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "nonexistent.py") #print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "lorem.txt") #print("Result for current directory:")
     print(result)
     print("")
 
